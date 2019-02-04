1fb9e0a2dbb9ab060c0989963e7d30bf0946835d
jd2rogers2 <jd2rogers2@gmail.com>
2019-02-04 21:01:53 +0900

    using postgres for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_12_120711) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
     t.boolean "submitted"
